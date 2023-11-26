1cbd620778da89b2de468f06b96232a9e4c9b6cb
lonely0811 <108106278+lonely0811@users.noreply.github.com>
2023-11-26 07:40:00 +0700

    Create hyperweb.js

---
+++ hyperweb.js
@@ -0,0 +1,20 @@
+[rewrite_local]
+https?:\/\/zy6kcqa01a\.execute-api\.us-east-2\.amazonaws\.com\/prod\/verifyReceipt url script-response-body https://raw.githubusercontent.com/chxm1023/Rewrite/main/Hyperweb.js
+
+[mitm]
+hostname = zy6kcqa01a.execute-api.us-east-2.amazonaws.com
+
+*************************************/
+
+
+var chxm1023 = JSON.parse($response.body);
+
+chxm1023 = {
+  "autoRenewStatus" : "1",
+  "isActive" : true,
+  "expireDateMs" : 4092599349000,
+  "expirationIntent" : null,
+  "productId" : "ai.laso.ios.HyperWeb.yearly.subscription"
+};
+
+$done({body : JSON.stringify(chxm1023)});
