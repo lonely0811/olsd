5f94aabc4dea797cb9e1f3824fd6ba5d96abb603
lonely0811 <108106278+lonely0811@users.noreply.github.com>
2023-12-14 16:48:30 +0700

    Create nice1.js

---
+++ nice1.js
@@ -0,0 +1,7 @@
+**/
+
+var Q = JSON.parse($response.body);
+Q.data.user.lifetime_subscription = true;
+Q.data.user.store_subscription = true;
+Q.data.user.subscription = true;
+$done({body : JSON.stringify(Q)});
