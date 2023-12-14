6753bb4ffd92c3248019c268fd81fa2007bba03f
lonely0811 <108106278+lonely0811@users.noreply.github.com>
2023-12-14 12:24:07 +0700

    Create tele.js

---
+++ tele.js
@@ -0,0 +1,22 @@
+{
+  "status": 200,
+  "data": {
+    "user": {
+      "id": 1400064,
+      "last_ads_view": 0,
+      "next_daily_reward_claim_time": 1702531201,
+      "last_getting_daily_reward": 0,
+      "telegramAuthToken": "4f8eae6d9df655e2b9ae6cc22fde0982128abe7a102b209fdb2c1942afd7cfab",
+      "referral_link": "https:\/\/t.me\/NicegramConnectBot?start=2da1b872d6eacda53ccb773c34c67204c77e8d40152907c22224758ad9c4a5f5",
+      "store_subscription": true,
+      "user_name": "",
+      "first_name": "HẢI",
+      "gems_balance": 3,
+      "subscription": false,
+      "server_time": 1702531202,
+      "last_name": "KIỀU",
+      "lifetime_subscription": true
+    }
+  },
+  "message": ""
+}
