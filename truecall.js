91eb071a1169138ded40b5f0647caf21b32ee4b6
lonely0811 <108106278+lonely0811@users.noreply.github.com>
2024-01-07 09:50:38 +0700

    Update truecall.js

--- truecall.js
+++ truecall.js
@@ -8,13 +8,15 @@ const features = [
   { id: "caller_id", rank: -2147483648, status: "Included" },
   { id: "siri_search", rank: 1, status: "Included" },
   { id: "no_ads", rank: 2, status: "Included" },
-  { id: "call_assistant", rank: 2, status: "Included" },
   { id: "extended_spam_blocking", rank: 3, status: "Included" },
-  { id: "who_viewed_my_profile", rank: 4, status: "Included" },
-  { id: "incognito_mode", rank: 6, status: "Included" },
-  { id: "premium_badge", rank: 10, status: "Included" },
+  { id: "ct_call_recording", rank: 7, status: "Included" },
+  { id: "call_assistant", rank: 2, status: "Included" },
+  { id: "who_viewed_my_profile", rank: 9, status: "Included" },
+  { id: "incognito_mode", rank: 11, status: "Included" },
+  { id: "premium_badge", rank: 15, status: "Included" },
   { id: "premium_support", rank: 11, status: "Included" },
-  { id: "gold_caller_id", rank: 13, status: "Included" }
+  { id: "gold_caller_id", rank: 20, status: "Included" }
+  { id: "premium_app_icon", rank: 19, status: "Included" },
 ];
 var obj;
 switch ($request.url) {
