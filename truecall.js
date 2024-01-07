bde027afef74a773ed5417bf61fba36e2ad9b202
lonely0811 <108106278+lonely0811@users.noreply.github.com>
2024-01-07 10:43:27 +0700

    Update truecall.js

--- truecall.js
+++ truecall.js
@@ -22,7 +22,7 @@ var obj;
 switch ($request.url) {
   case findUrl(/subscriptions\/status/):
       obj = {
-        expire: "2030-09-06T11:20:25Z",
+        expire: "2030-01-01T01:01:01Z",
         start: "2024-01-07T02:32:04Z",
         paymentProvider: "Apple",
         isExpired: false,
