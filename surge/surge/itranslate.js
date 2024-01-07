8624766b6126e7e4b37c1e62481e3a5182a82c6f
lonely0811 <108106278+lonely0811@users.noreply.github.com>
2024-01-07 08:17:12 +0700

    Add files via upload

---
+++ surge/surge/itranslate.js
@@ -0,0 +1,13 @@
+let obj= {
+  "licenses": [
+    {
+      "product_id": "com.itranslate.pro.yearly",
+      "is_trial_period": false,
+      "original_transaction_id": "440001324215869",
+      "transaction_id": "440001324215869",
+      "expires_date_ms": 4099246456000,
+      "bundle_id": "com.outerspaceapps.itranslate"
+    }
+  ]
+}
+$done({body: JSON.stringify(obj)});
---
+++ surge/surge/revenuecat.js
@@ -0,0 +1,39 @@
+const resp = {};
+const obj = JSON.parse(typeof $response != "undefined" && $response.body || null);
+
+const ua = $request.headers['User-Agent'] || $request.headers['user-agent'];
+const list = {
+	'VSCO': { name: 'membership', id: 'com.circles.fin.premium.yearly' },
+	'1Blocker': { name: 'premium', id: 'blocker.ios.subscription.yearly' },
+	'Anybox': { name: 'pro', id: 'cc.anybox.Anybox.annual' },
+	'Fileball': { name: 'filebox_pro', id: 'com.premium.yearly' },
+	'ipTV': { name: 'ipTV +', id: 'iptv_9.99_1y_7d_free' },
+	'APTV': { name: 'pro', id: 'com.kimen.aptvpro.lifetime' },
+	'Blink': { name: 'pro', id: 'blink_shell_plus_1y_1999' },
+        'mizframa': { name: 'premium', id: 'mf_20_lifetime2' },
+        'CallRecorder': { name: 'subscriptions', id: 'com.prettyboa.CallRecorder.MonthlySubscription2999' }
+};
+const data = {
+	"expires_date": "2099-02-18T07:52:54Z",
+	"original_purchase_date": "2020-02-11T07:52:55Z",
+	"purchase_date": "2020-02-11T07:52:54Z"
+};
+
+if (typeof $response == "undefined") {
+	delete $request.headers["x-revenuecat-etag"]; // prevent 304 issues
+	delete $request.headers["X-RevenueCat-ETag"];
+	resp.headers = $request.headers;
+} else if (obj && obj.subscriber) {
+	obj.subscriber.subscriptions = obj.subscriber.subscriptions || {};
+	obj.subscriber.entitlement = obj.subscriber.entitlement || {};
+	for (const i in list) {
+		if (new RegExp(`^${i}`, `i`).test(ua)) {
+			obj.subscriber.subscriptions[list[i].id] = data;
+			obj.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+			obj.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+			break;
+		}
+	}
+	resp.body = JSON.stringify(obj).replace(/\"expires_date\":\"\w{4}/g, "\"expires_date\":\"2099").replace(/\"period_type\":\"\w+\"/g, "\"period_type\":\"active\"");
+}
+$done(resp);
