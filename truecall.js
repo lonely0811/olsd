e1f1dec972bbcea97fbcc1a432a272b2f99db2dd
lonely0811 <108106278+lonely0811@users.noreply.github.com>
2024-01-07 08:47:43 +0700

    Update truecall.js

--- truecall.js
+++ truecall.js
@@ -6,7 +6,7 @@ function findUrl(_reg) {
 const features = [
   { id: "spam_blocking", rank: -2147483648, status: "Included" },
   { id: "caller_id", rank: -2147483648, status: "Included" },
-  { id: "live_caller_id", rank: 5, status: "Included" },
+  { id: "live_caller_id", rank: -2147483648, status: "Included" },
   { id: "no_ads", rank: 1, status: "Included" },
   { id: "call_assistant", rank: 2, status: "Included" },
   { id: "extended_spam_blocking", rank: 3, status: "Included" },
