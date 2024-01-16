a8941fcfd1da9720773eb19c9f0464ccfe6781e5
lonely0811 <108106278+lonely0811@users.noreply.github.com>
2024-01-16 14:31:48 +0700

    Update nice.js

--- nice.js
+++ nice.js
@@ -1,5 +1,4 @@
 const url = $request.url;
-const isQX = typeof $task !== "undefined";
 var chxm1023 = JSON.parse($response.body);
 const subscriptionTest = /https:\/\/nicegram\.cloud\/api\/v\d\/user\/info/;
 const premiumTest = /https:\/\/restore-access\.indream\.app\/restoreAccess/;
@@ -15,10 +14,6 @@ if (subscriptionTest.test(url)) {
 
 if (premiumTest.test(url)) {
   chxm1023["data"] = {"premiumAccess": true};
-}
-
-function finalizeResponse(content) {
-  return { status: isQX ? "HTTP/1.1 200 OK" : 200, headers: $response.headers, body: JSON.stringify(content) };
 }
 
 $done(isQX ? finalizeResponse(chxm1023) : chxm1023);
