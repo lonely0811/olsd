62ac9cfa5d702b8a66b5fc3ccf373ff11e096154
lonely0811 <108106278+lonely0811@users.noreply.github.com>
2024-05-12 11:35:31 +0700

    Update Tansingbox.js

--- Singbox/Rule/Tansingbox.js
+++ Singbox/Rule/Tansingbox.js
@@ -1,166 +1,205 @@
 {
-  "log": {
-    "level": "info",
-    "timestamp": true
-  },
-  "experimental": {
-    "clash_api": {
-      "external_controller": "127.0.0.1:9090",
-      "external_ui": "ui",
-      "secret": "",
-      "external_ui_download_url": "https://mirror.ghproxy.com/https://github.com/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.zip",
-      "external_ui_download_detour": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁",
-      "default_mode": "rule"
+    "log": {
+        "disabled": false,
+        "level": "info",
+        "timestamp": true
     },
-    "cache_file": {
-      "enabled": true,
-      "store_fakeip": true
+    "experimental": {
+        "clash_api": {
+            "external_controller": "127.0.0.1:9090",
+            "external_ui": "ui",
+            "secret": "",
+            "external_ui_download_url": "https://mirror.ghproxy.com/https://github.com/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.zip",
+            "external_ui_download_detour": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁",
+            "default_mode": "rule"
+        },
+        "cache_file": {
+            "enabled": true,
+            "store_fakeip": true
+        }
+    },
+    "dns": {
+        "servers": [{
+            "tag": "direct-dns",
+            "address": "local",
+            "strategy": "ipv4_only",
+            "detour": "direct"
+        },
+            {
+                "tag": "select-dns",
+                "address": "8.8.4.4",
+                "strategy": "ipv4_only",
+                "detour": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁"
+            },
+            {
+                "tag": "block",
+                "address": "rcode://success"
+            },
+            {
+                "tag": "block-dns",
+                "address": "rcode://success"
+            }],
+        "rules": [{
+            "domain_suffix": [
+                ".arpa.",
+                ".arpa"
+            ],
+            "server": "block-dns",
+            "rewrite_ttl": 20
+        },
+            {
+                "rule_set": "ADSVN",
+                "server": "block"
+            },
+            {
+                "network": "udp",
+                "port": 443,
+                "server": "block-dns",
+                "rewrite_ttl": 20
+            },
+            {
+                "outbound": "any",
+                "server": "localDns",
+                "disable_cache": true
+            },
+            {
+                "outbound": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁",
+                "server": "select-dns",
+                "rewrite_ttl": 20
+            },
+            {
+                "outbound": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁",
+                "server": "select-dns",
+                "rewrite_ttl": 20
+            }],
+        "reverse_mapping": true,
+        "strategy": "ipv4_only",
+        "independent_cache": true
+    },
+    "inbounds": [{
+        "type": "tun",
+        "tag": "tun-in",
+        "interface_name": "tun0",
+        "inet4_address": "172.19.0.1/30",
+        "mtu": 9000,
+        "auto_route": true,
+        "strict_route": true,
+        "stack": "system",
+        "endpoint_independent_nat": true,
+        "sniff": false
+    },
+        {
+            "listen": "0.0.0.0",
+            "listen_port": 2080,
+            "sniff": false,
+            "tag": "mixed-in",
+            "type": "mixed"
+        }],
+    "outbounds": [{
+        "tag": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁",
+        "type": "selector",
+        "outbounds": [
+            "🅐🅤🅣🅞 🅥🅝🅐🅜",
+            "🅐🅤🅣🅞 🅢🅘🅝🅖",
+            "🅐🅤🅣🅞 🅗🅚",
+            "🅓🅘🅡🅔🅒🅣"]
+    },
+        {
+            "tag": "🅐🅤🅣🅞 🅥🅝🅐🅜",
+            "type": "urltest",
+            "outbounds": [],
+            "url": "http://www.gstatic.com/generate_204",
+            "interval": "5m",
+            "tolerance": 50
+        },
+        {
+            "tag": "🅐🅤🅣🅞 🅢🅘🅝🅖",
+            "type": "urltest",
+            "outbounds": [],
+            "url": "http://www.gstatic.com/generate_204",
+            "interval": "5m",
+            "tolerance": 50
+        },
+        {
+            "tag": "🅐🅤🅣🅞 🅗🅚",
+            "type": "urltest",
+            "outbounds": [],
+            "url": "http://www.gstatic.com/generate_204",
+            "interval": "5m",
+            "tolerance": 50
+        },
+        {
+            "tag": "🅓🅘🅡🅔🅒🅣",
+            "type": "direct"
+        },
+        {
+            "tag": "bypass",
+            "type": "direct"
+        },
+        {
+            "tag": "block",
+            "type": "block"
+        },
+        {
+            "tag": "dns-out",
+            "type": "dns"
+        }],
+    "route": {
+        "auto_detect_interface": true,
+        "override_android_vpn": true,
+        "final": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁",
+        "rules": [{
+            "network": "udp",
+            "port": 443,
+            "outbound": "block"
+        },
+            {
+                "domain_suffix": [
+                    "googlesyndication.com"
+                ],
+                "outbound": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁"
+            },
+            {
+                "ip_cidr": [
+                    "8.8.8.8"
+                ],
+                "outbound": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁"
+            },
+            {
+                "inbound": [
+                    "dns-in"
+                ],
+                "outbound": "dns-out"
+            },
+            {
+                "rule_set": [
+                    "ADSVN"
+                ],
+                "outbound": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁"
+            },
+            {
+                "outbound": "dns-out",
+                "port": [
+                    53
+                ]
+            },
+            {
+                "ip_cidr": [
+                    "224.0.0.0/3",
+                    "ff00::/8"
+                ],
+                "outbound": "block",
+                "source_ip_cidr": [
+                    "224.0.0.0/3",
+                    "ff00::/8"
+                ]
+            }],
+        "rule_set": [{
+            "tag": "ADSVN",
+            "type": "remote",
+            "format": "source",
+            "url": "https://raw.githubusercontent.com/DrStrangeVN/Singbox/main/Rule.json",
+            "download_detour": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁"
+        }]
     }
-  },
-  "dns": {
-    "fakeip": {
-      "enabled": true,
-      "inet4_range": "198.18.0.0/15"
-    },
-	"servers": [
-      {
-        "address": "8.8.4.4",
-        "address_resolver": "dns-direct",
-        "strategy": "prefer_ipv4",
-        "tag": "dns-remote"
-      },
-      {
-        "address": "local",
-        "address_resolver": "dns-local",
-        "detour": "direct",
-        "strategy": "prefer_ipv4",
-        "tag": "dns-direct"
-      },
-      {
-        "tag": "fakeip",
-        "address": "fakeip"
-      },
-	  {
-		"tag": "block-dns",
-		"address": "rcode://success"
-	  }
-	],
-  "rules": [
-      {
-        "domain_suffix": [
-          ".arpa.",
-          ".arpa"
-        ],
-        "server": "block-dns",
-        "rewrite_ttl": 20
-      },
-      {
-        "outbound": "direct",
-        "server": "dns-direct",
-        "rewrite_ttl": 20
-      },
-      {
-        "outbound": "any",
-        "server": "dns-direct",
-        "disable_cache": true
-      },
-      {
-        "inbound": "tun",
-        "query_type": [
-          "A",
-          "AAAA"
-        ],
-        "server": "fakeip",
-        "rewrite_ttl": 1
-      },
-      {
-        "rule_set": [
-          "ADSVN"
-        ],
-        "server": "block-dns",
-        "disable_cache": true
-      }
-    ],
-    "reverse_mapping": true,
-    "strategy": "ipv4_only",
-    "independent_cache": true,
-    "disable_cache": true
-  },
-  "inbounds": [
-	{
-	  "type": "tun",
-	  "tag": "tun-in",
-	  "interface_name": "tun0",
-	  "inet4_address": "172.19.0.1/30",
-	  "mtu": 9000,
-	  "auto_route": true,
-	  "strict_route": true,
-	  "stack": "system",
-	  "endpoint_independent_nat": true,
-	  "sniff": true
-	},
-	{
-	  "listen": "0.0.0.0",
-	  "listen_port": 2080,
-	  "sniff": false,
-	  "tag": "mixed-in",
-	  "type": "mixed"
-	}
-  ],
-  "outbounds": [
-		{
-			"tag": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁",
-			"type": "selector",
-			"outbounds": ["direct"]
-		},
-		{
-			"tag": "direct",
-			"type": "direct"
-		},
-		{
-			"tag": "bypass",
-			"type": "direct"
-		},
-		{
-			"tag": "block",
-			"type": "block"
-		},
-		{
-			"tag": "dns-out",
-			"type": "dns"
-		}
-	],
-  "route": {
-	"auto_detect_interface": true,
-	"final": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁",
-	"rules": [
-	  {
-		"inbound": [
-		  "dns-in"
-		],
-		"outbound": "dns-out"
-	  },
-	  {
-		  "rule_set": [
-			"ADSVN"
-		  ],
-		  "outbound": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁"
-	  },
-	  {
-		"outbound": "dns-out",
-		"port": [
-		  53
-		]
-	  }
-	 ],
-	"rule_set": [
-		{
-		  "tag": "ADSVN",
-		  "type": "remote",
-		  "format": "source",
-		  "url": "https://raw.githubusercontent.com/DrStrangeVN/Singbox/main/Rule.json",
-		  "download_detour": "🅵🆄🅻🅻 🆂🅴🆁🆅🅴🆁"
-	  }
-	]
-      }
-   }
+}
